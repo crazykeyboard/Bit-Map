871fa567bb8cf6ef0bd457a0b556254d81f24646
crazykeyboard <doufei_2012@163.com>
2013-09-02 13:44:34 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "multiBitmap.h"
+using namespace  std;
+
+int main(int argc, char** argv)
+{
+	CMultiBitMap testBitset(100) ;
+	cout << "num of one£º" << testBitset.oneCount() << endl;
+	testBitset.setAll(true) ;
+	cout << "num of one£º" << testBitset.oneCount() << endl;
+	testBitset.setAll(false) ;
+	cout << "num of one£º" << testBitset.oneCount() << endl;
+
+
+
+	testBitset.setBit(0, true) ;
+	cout << "num of one£º" << testBitset.oneCount() << endl;
+	testBitset.setBit(99, true) ;
+	cout << "num of one£º" << testBitset.oneCount() << endl;
+
+	if (testBitset.getBit(0))
+		cout << "OK" << endl;
+	if (testBitset.getBit(99))
+		cout << "OK" << endl;
+
+	testBitset.resize(200) ;
+	cout << "num of one£º" << testBitset.oneCount() << endl;
+
+	testBitset.setAll(true) ;
+	cout << "num of one£º" << testBitset.oneCount() << endl;
+	testBitset.setAll(false) ;
+	cout << "num of one£º" << testBitset.oneCount() << endl;
+
+	testBitset.setBit(0, true) ;
+	cout << "num of one£º" << testBitset.oneCount() << endl;
+	testBitset.setBit(199, true) ;
+	cout << "num of one£º" << testBitset.oneCount() << endl;
+
+	if (testBitset.getBit(0))
+		cout << "OK" << endl;
+	if (testBitset.getBit(199))
+		cout << "OK" << endl;
+
+
+	return 0 ;
+}
